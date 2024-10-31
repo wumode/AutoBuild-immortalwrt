801cbb1d16f7508a38750e67b73e9cafc00a5379
wumode <wumo1999@gmail.com>
2024-10-31 16:23:40 +0800

    add go 23

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,6 +25,9 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
